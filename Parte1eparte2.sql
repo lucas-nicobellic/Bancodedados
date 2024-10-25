5a419f85966d9922c00ba60b916b7b0170438c93
lucas-nicobellic <luhvalentim04@gmail.com>
2024-10-25 19:39:40 -0300

    TAREFA: AC2 PARCIAL - BANCO DE DADOS | PARTE 1 E 2

---
+++ Parte1eparte2.sql
@@ -0,0 +1,7 @@
+INSERT INTO Produtos (nome, preco, estoque, perecivel, marca, nacionalidade)
+VALUES 
+('Leite Integral', 4.50, 100, TRUE, 'Marca A', 'Brasil'),
+('Arroz Branco', 20.00, 50, FALSE, 'Marca B', 'Brasil'),
+('Chocolate', 7.90, 200, TRUE, 'Marca C', 'Suiça'),
+('Cerveja', 3.99, 150, TRUE, 'Marca D', 'Alemanha'),
+('Sabonete', 2.50, 300, FALSE, 'Marca E', 'EUA');
